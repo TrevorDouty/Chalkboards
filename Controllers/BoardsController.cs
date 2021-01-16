341a10881999f8b0fb938b808d9b1319a1f1d25e
TrevorDouty <tdouty@nnu.edu>
2021-01-15 17:51:19 -0700

    first init

---
+++ Controllers/BoardsController.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace chalkboards.Controllers
+{
+  public class BoardsController : ControllerBase
+  {
+
+  }
+}
---
+++ Models/Board.cs
@@ -0,0 +1,11 @@
+namespace chalkboards.Models
+{
+  public class Board
+  {
+    public string Img { get; set; }
+    public string Description { get; set; }
+    public string Title { get; set; }
+    public int Quantity { get; set; }
+    public int Price { get; set; }
+  }
+}
