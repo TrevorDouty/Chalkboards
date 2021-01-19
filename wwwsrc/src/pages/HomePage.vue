43bf6e08b98d2a93480d1c144be076974bc184fb
TrevorDouty <tdouty@nnu.edu>
2021-01-19 11:57:00 -0700

    Home page design

--- wwwsrc/src/pages/HomePage.vue
+++ wwwsrc/src/pages/HomePage.vue
@@ -1,9 +1,13 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <img src="https://bcw.blob.core.windows.net/public/img/8600856373152463" alt="CodeWorks Logo">
-    <h1 class="my-5 bg-dark text-light p-3 rounded d-flex align-items-center">
-      <span class="mx-2 text-white">Vue 3 Starter</span>
+
+  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center bg-dark row">
+<div class="border border-white rounded">
+
+    <h1 class="my-5 bg-dark text-light p-3 rounded  text-center">
+      <div class="mx-2 text-white">Brie's Boards</div>
     </h1>
+      <p class="text-light">Creating beautiful designs to meet your event needs.</p>
+</div>
   </div>
 </template>
 
