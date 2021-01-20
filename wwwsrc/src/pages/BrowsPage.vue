48cb10e5169def9be8a74fd486489d6775cb11ed
TrevorDouty <tdouty@nnu.edu>
2021-01-20 14:06:41 -0700

    Browse page and browse component

---
+++ wwwsrc/src/pages/BrowsPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="browse-page">
+<browse-component v-for="board in boards" :key="board.id" :props: board-prop/>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'BrowsePage',
+  setup(){
+    return {}
+  },
+  components:{}
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
