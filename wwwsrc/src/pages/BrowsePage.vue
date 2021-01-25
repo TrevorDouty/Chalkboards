3cabcd328a1cd64bd42254108fa510ad2ded9cba
TrevorDouty <tdouty@nnu.edu>
2021-01-25 16:30:35 -0700

    Working on Browse page. Images

--- wwwsrc/src/pages/BrowsePage.vue
+++ wwwsrc/src/pages/BrowsePage.vue
@@ -1,19 +1,30 @@
 <template>
-  <div class="browse-page container-fluid">
-    <div class="row">
+  <div class="browse-page container-fluid bg-dark text-light underline">
+    <div class="row pb-2">
       <div class="col text-center">
         <h1>Browse Chalkboards</h1>
       </div>
     </div>
-    <div class="row">
-      <div class="col-3">
+    <div class="row justify-content-around">
+      <div class="col-3 text-center">
         <h3>
           Events
         </h3>
-<img src="../assets/img/Baby.png" alt="">
+<img src="../assets/img/Baby1.png" alt="" class="img-fluid pt-2">
 </div>
-      <div class="col-3"></div>
-      <div class="col-3"></div>
+      <div class="col-3 text-center">
+        <h3>Holidays</h3>
+        <img src="../assets/img/holiday.png" alt="" class="img-fluid pt-5">
+      </div>
+      <div class="col-3 text-center">
+        <h3>Inspirational</h3>
+        <img src="../assets/img/inspire.png" alt="" class="img-fluid">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col text-center">
+        <p>Click On Categories To View More Examples</p>
+      </div>
     </div>
   </div>
 </template>
@@ -30,5 +41,7 @@ export default {
 
 
 <style lang="scss" scoped>
-
+.underline{
+  text-decoration: underline;
+}
 </style>
